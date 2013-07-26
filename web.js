cf1f79b37f1816122fa7ab0760f36b7bc46dbef0
Shuhan Majid <shuhanmajid@hotmail.com>
2013-07-26 20:23:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
+var buffer= fs.readFileSync('index.html');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
